262d83a75c80e17d5dff6213715a3494eb55877d
junaidsh <junaidsh@INjunaidsh.local>
2016-12-23 08:17:22 +0530

    Added the files to configure chef solo master mesos

---
+++ chef_solo_master.sh
@@ -0,0 +1,6 @@
+curl -L https://www.opscode.com/chef/install.sh | bash
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
+git clone https://github.com/Junaidshah/teralytics.git /var/chef-solo
+mkdir -p /var/log/chef
+touch /var/log/chef/client.log
+/usr/bin/chef-solo -c /var/chef-solo/solo.rb -j /var/chef-solo/solo.json > /var/log/chef/client.log 2>&1 
